0f1c62a16c493eb73728a89af989f46fa3c8bcf8
AnnaPashkina <anya230489@gmail.com>
2022-12-10 12:54:49 +0200

    From lesson 28

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("Hello Anna!");
+        /*int my_number = 5;
+        double second_number;
+        System.out.println("my number is" + my_number);
+        second_number=22.5;
+        System.out.println("my nymber is" + second_number);
+        String my_string = "My first String";
+        System.out.println(my_string);
+        System.out.println(my_string.toLowerCase());
+        System.out.println(my_string.toUpperCase());
+        System.out.println(my_string.length());
+        System.out.println(my_string.charAt(0));*/
+        int a = 10;
+        int b = 3;
+/*
+>
+<
+>=
+<=
+
+ */
+        /*System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+        System.out.println(a==b);
+        System.out.println(a!=b);*/
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("You are an old person");
+            }
+        int weekDay = 6;
+
+        else {
+            System.out.println("You are a child");
+        } switch (weekDay) {
+            case 1:
+                System.out.println ("Monday");}
+        }
+
     }
-}
